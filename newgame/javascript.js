1cf64d2e2c81f62b4251fd9b9906e62e1e4bce6e
Faisal-ZaheerVI <faisalszaheer@gmail.com>
2017-02-23 17:31:10 -0800

    Update javascript.js

--- newgame/javascript.js
+++ newgame/javascript.js
@@ -6,7 +6,7 @@ playerImg.src = "imgs/flyman.png";
 
 function startGame() {
     game.start();
-    player = new Player(90, 90, "red", 100, 120);
+    player = new Player(100, 100, "red", 10, 120);
 }
 
 var game = {
@@ -16,14 +16,47 @@ var game = {
         this.canvas.height = 720;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.interval = setInterval(updateGameArea, 20);
+        window.addEventListener('keydown', function (e) {
+            game.keys = (game.keys || []);
+            game.keys[e.keyCode] = (e.type == "keydown");
+        })
+        window.addEventListener('keyup', function (e) {
+            game.keys[e.keyCode] = (e.type == "keydown");            
+        })
+    }, 
+    clear : function(){
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     }
 }
 
 function Player(width, height, color, x, y) {
+    this.gamearea = game;
     this.width = width;
     this.height = height;
+    this.speedX = 0;
+    this.speedY = 0;    
     this.x = x;
     this.y = y;    
-    ctx = game.context;
-    ctx.drawImage(playerImg, this.x, this.y, this.width, this.height);
+    this.update = function() {
+        ctx = game.context;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+        ctx.drawImage(playerImg, this.x, this.y, this.width, this.height);
+    }
+    this.move = function() {
+        this.x += this.speedX;
+        this.y += this.speedY;        
+    }    
+}
+
+function updateGameArea() {
+    game.clear();
+    player.speedX = 0;
+    player.speedY = 0;    
+    if (game.keys && game.keys[37] || game.keys[37]) {player.speedX = -2; }
+    if (game.keys && game.keys[39]) {player.speedX = 2; }
+    if (game.keys && game.keys[38]) {player.speedY = -2; }
+    if (game.keys && game.keys[40]) {player.speedY = 2; }
+    player.move();    
+    player.update();
 }
